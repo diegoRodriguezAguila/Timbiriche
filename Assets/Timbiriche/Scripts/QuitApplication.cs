73efcc4bdc7a8182c8eb2be844d759a71592c145
diegoRodriguezAguila <diroag@gmail.com>
2016-10-26 09:58:13 -0400

    Improved back button

--- Assets/Timbiriche/Scripts/QuitApplication.cs
+++ Assets/Timbiriche/Scripts/QuitApplication.cs
@@ -1,20 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class QuitApplication : MonoBehaviour {
-
-	public void Quit()
-	{
-		//If we are running in a standalone build of the game
-	#if UNITY_STANDALONE
-		//Quit the application
-		Application.Quit();
-	#endif
-
-		//If we are running in the editor
-	#if UNITY_EDITOR
-		//Stop playing the scene
-		UnityEditor.EditorApplication.isPlaying = false;
-	#endif
-	}
+public class QuitApplication : MonoBehaviour
+{
+    public void Quit()
+    {
+        //If we are running in a standalone build of the game
+        Application.Quit();
+        //If we are running in the editor
+#if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
 }
