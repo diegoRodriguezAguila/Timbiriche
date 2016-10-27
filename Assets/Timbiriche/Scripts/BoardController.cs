e52a6c2a6a35df4ac7f2ef99bb9394c3bb672a64
diegoRodriguezAguila <diroag@gmail.com>
2016-10-27 12:14:22 -0400

    added tiles for board

---
+++ Assets/Timbiriche/Scripts/BoardController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+    public int boardSize;
+    public GameObject cellPrefab;
+
+    void OnLevelWasLoaded()
+    {
+        GenerateBoard();
+    }
+
+    void GenerateBoard()
+    {
+        for (var row = 0; row < boardSize; row++)
+        {
+            for (var column = 0; column < boardSize; column++)
+            {
+                Instantiate(cellPrefab, new Vector3(row, column, 0), Quaternion.identity);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
