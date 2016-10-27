30de4f93969d1ff64e0baf93f616a2a6a52863ff
diegoRodriguezAguila <diroag@gmail.com>
2016-10-27 14:49:21 -0400

    Added click listeners for the 4 tile parts

---
+++ Assets/Timbiriche/Scripts/TileController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    private PolygonCollider2D[] colliders;
+    // Use this for initialization
+    void Start()
+    {
+        colliders = GetComponents<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnMouseDown()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var hitCollider = Physics2D.OverlapPoint(mousePosition);
+
+        Debug.Log("mouse pos " + mousePosition.x + " y " + mousePosition.y + " ");
+
+
+        if (hitCollider)
+        {
+            Debug.Log("Hit " + hitCollider.transform.name + " x" + hitCollider.transform.position.x + " y " +
+                      hitCollider.transform.position.y);
+            if (hitCollider == colliders[0])
+                Debug.Log("COLLIDER 0: LEFT");
+            if (hitCollider == colliders[1])
+                Debug.Log("COLLIDER 1: BOTTOM");
+            if (hitCollider == colliders[2])
+                Debug.Log("COLLIDER 2: TOP");
+            if (hitCollider == colliders[3])
+                Debug.Log("COLLIDER 3: RIGHT");
+        }
+    }
+}
